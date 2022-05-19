96ef2a444e4746aaf3ac0f9f185bf37a35118d3d
jorgedu87 <jorgedu87@outlook.com>
2022-05-19 16:49:20 -0300

    finalizado

--- TareaClase7/Program.cs
+++ TareaClase7/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+int[] numero = new int[10];
+int contador = 0;
+int sumatoria = 0;
+decimal promedio = 0M;
+int mayor = 0;
+int menor = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    contador++;
+    Console.WriteLine($"Ingrese el numero {contador}");
+    numero[i] = int.Parse(Console.ReadLine());
+    sumatoria = sumatoria + numero[i];
+}
+
+menor = numero[0];
+for (int i = 0; i < 10; i++)
+{
+    if (numero[i] > mayor)
+    { mayor = numero[i]; }
+    if (numero[i] < menor)
+    { menor = numero[i]; }
+    Console.WriteLine($"Los numeros ingresados fueron:  { numero[i]}");
+}
+
+Console.WriteLine($"La sumatoria de los numeros ingresados es igual a  { sumatoria}");
+promedio = sumatoria / 10;
+Console.WriteLine($"El promedio de los numeros ingresados es igual a  { promedio}");
+Console.WriteLine($"El numero mayor ingresado es igual a  { mayor}");
+Console.WriteLine($"El numero menor ingresado es igual a  { menor}");
+
+
+
+
